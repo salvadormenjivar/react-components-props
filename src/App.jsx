e3ef33225b619b3858fe61f738e08a178d6b4d1b
salvador <salvador@debian12.home>
2025-01-07 06:06:05 -0600

    Updating readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import ContactList from "./components/ContactList";
+import Avatar from "./components/Avatar";
 
 function App() {
   return (
     <div>
+      <Avatar img="https://media.licdn.com/dms/image/v2/D4E03AQF1209wRgnGog/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1731618995424?e=1741824000&v=beta&t=zQ51fPgDBWTaiK89C5SnqUeKDL_6Yu6D5x41IIY9GTk"/>
+      <h1>Salvador Peña</h1>
       <h1 className="heading">My Contacts</h1>
       <ContactList />
     </div>
---
+++ src/components/Details.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Details(props){
+    return (
+    <>
+    <p className="info">{props.detailInfo}</p>    
+    </>
+    );
+}
+
+export default Details;
